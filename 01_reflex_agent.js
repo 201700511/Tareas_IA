aae884e861d9d7d506ef16dcd2f4eb454cc8393a
Juan Pablo Alvarado Velásquez <3005421270101@ingenieria.usac.edu.gt>
2021-08-01 23:06:56 -0600

    Tarea 1
    Modificar codigo actual (visita 5 estados) para que visita los 8 estados estipulados

---
+++ 01_reflex_agent.js
@@ -0,0 +1,33 @@
+// MIT License
+// Copyright (c) 2020 Luis Espino
+
+function reflex_agent(location, state){
+    if (state=="DIRTY") return "CLEAN";
+    else if (location=="A") return "RIGHT";
+    else if (location=="B") return "LEFT";
+}
+
+var add_state = false
+function test(states){
+       var location = states[0];		
+       var state = states[0] == "A" ? states[1] : states[2];
+       var action_result = reflex_agent(location, state);
+       document.getElementById("log").innerHTML+="<br>Location: ".concat(location).concat(" | Action: ").concat(action_result);
+       if (action_result == "CLEAN"){
+         if (location == "A") states[1] = "CLEAN";
+          else if (location == "B") states[2] = "CLEAN";
+       }
+       else if (action_result == "RIGHT") states[0] = "B";
+       else if (action_result == "LEFT") states[0] = "A";	
+       
+       if(states[1] == "CLEAN" && states[2] == "CLEAN" && location == "B" && add_state == false){
+        states[1] = "DIRTY"
+        states[2] = "DIRTY"
+        add_state = true
+       }
+
+ setTimeout(function(){ test(states); }, 2000);
+}
+
+var states = ["A","DIRTY","DIRTY"];
+test(states);
